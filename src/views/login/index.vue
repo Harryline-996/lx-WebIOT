b610e2fd86ee3f8f6e359a4695ef716615e58cf8
LiXiang <865250792@qq.com>
2021-06-13 16:54:34 +0800

    update the background of login view

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">登录界面</h3>
       </div>
 
       <el-form-item prop="username">
@@ -180,6 +180,7 @@ $light_gray:#eee;
 .login-container {
   min-height: 100%;
   width: 100%;
+  background-image:url(../../assets/loginbg.jpg);
   background-color: $bg;
   overflow: hidden;
 
