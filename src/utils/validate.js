2034422a4e6fe776616c2701c2ba1693b393731b
LiXiang <865250792@qq.com>
2021-06-16 20:29:35 +0800

    add some function to validate the password and email

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -18,3 +18,32 @@ export function validUsername(str) {
   const valid_map = ['admin', 'editor']
   return valid_map.indexOf(str.trim()) >= 0
 }
+
+/**
+ * 验证邮箱格式是否正确
+ */
+export function validEmail(value) {
+  const reg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/
+  return reg.test(value)
+}
+
+/**
+ * 过滤特殊字符
+ */
+export function stringFilter(str) {
+  const pattern = new RegExp('[`~!@#$^&*()=|{}\':;\',\\[\\].<>/?~！@#￥……&*（）&;—|{ }【】‘；：”“\'。，、？]')
+  let rs = ''
+  for (let i = 0; i < str.length; i++) {
+    rs = rs + str.substr(i, 1).replace(pattern, '')
+  }
+  return rs
+}
+
+/**
+ * 验证密码格式是否为6-20位的字母+数字
+ */
+export function validPass(value) {
+  const reg = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,20}$/
+  return reg.test(value)
+}
+
